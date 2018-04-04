aa13544c066cb2f0e65cf8299910db27d641c5a8
wouter-at-communitynetworkz <wouter.banenmatch@gmail.com>
2018-04-04 13:42:25 +0200

    consumer project

---
+++ main/java/com/roundabout/logtologstashconsumer/Main.java
@@ -0,0 +1,25 @@
+package com.roundabout.logtologstashconsumer;
+
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args)
+            throws Exception
+    {
+        HttpInteractor httpInteractor = new HttpInteractor();
+        System.out.println(httpInteractor.logToLogstash("hola"));
+        System.out.println(httpInteractor.logToLogstash(2345));
+        String[] myStringArray;
+        myStringArray = new String[]{"a","b","c"};
+        System.out.println(httpInteractor.logToLogstash(myStringArray));
+        // GSON
+        Gson gson = new Gson();
+
+        for (String s : args) {
+            System.out.println("Args: " + s);
+        }
+
+        //System.out.println("Done : geson = " + gson.toString());
+        System.out.println("log to logstash");
+    }
+}
